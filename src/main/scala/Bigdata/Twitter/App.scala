d5597783b26c5749a564c88ae6cc1de768390704
Leonardo Freire <lfreire80@gmail.com>
2018-10-09 17:15:06 -0300

    Correção parametrização da pasta de chckpoint

--- src/main/scala/Bigdata/Twitter/App.scala
+++ src/main/scala/Bigdata/Twitter/App.scala
@@ -5,7 +5,7 @@ object App {
      
      val socketConfig = SocketConfig(args(0),args(1).toInt)
      
-     val elasticSearchConfig = ElasticSearchConfig("spark/twitter", "~/tmp/")
+     val elasticSearchConfig = ElasticSearchConfig("spark/twitter", args(2))
      
      val monitor = new Monitor(socketConfig, elasticSearchConfig);
      monitor.start()

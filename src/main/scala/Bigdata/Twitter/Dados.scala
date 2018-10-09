739d6c6e083467b788b63e551a16ac6b28fcb4a3
Leonardo Freire <lfreire80@gmail.com>
2018-10-09 17:17:23 -0300

    Remoção de imports indevidos

--- src/main/scala/Bigdata/Twitter/Dados.scala
+++ src/main/scala/Bigdata/Twitter/Dados.scala
@@ -1,5 +1,3 @@
 package Bigdata.Twitter
 
-import java.sql.Timestamp
-
 case class SocketData(text: String)
--- src/main/scala/Bigdata/Twitter/Tweety.scala
+++ src/main/scala/Bigdata/Twitter/Tweety.scala
@@ -1,5 +1,3 @@
 package Bigdata.Twitter
 
-import java.sql.Timestamp
-
 case class Tweety(hashTag: String, tweety: String)
